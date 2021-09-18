200d99bd45b1a3e9ad903d56554b6e275e0592b1
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-09-18 15:48:59 +0200

    some init layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import styles from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+export function NavBar({someValue="meh"}){
+  return <nav className={styles.nav}>
+    hello world
+  </nav>
 }
 
 export default MyApp
