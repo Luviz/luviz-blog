0d00484f096c5326a6ea74a2ce12d2a0ce9aed29
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-09-19 13:04:57 +0200

    responcive navbar

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,14 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
+import styles from "../styles/navBar.module.css"
+import Link from "next/link"
 
 export default function NavBar() {
     const li = [
         { text: "Home", value: "#" },
-        { text: "Blog", value: "#" },
-        { text: "About", value: "#" },
-    ].map((l, ix) => <li key={ix}><a href={l.value}>{l.text}</a></li>)
+        { text: "Blog", value: "#Blog" },
+        { text: "About", value: "#About" },
+    ].map((l, ix) => <li key={ix}><Link href={l.value} passHref>{l.text}</Link></li>)
 
-    return <nav className={"nav"}>
-        <img src="/Icon-BJ.svg" alt="Bardia Jedi Logo" className={"banner-icon"} />
+    return <nav className={styles.nav}>
+        <Link href="/" passHref>
+            <img src="/Icon-BJ.svg" alt="Bardia Jedi Logo" className={styles.bannerIcon} />
+        </Link>
         <ul>
             {li}
         </ul>
