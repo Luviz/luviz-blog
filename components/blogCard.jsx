2a3f6515f06e8d2896228dbbfbd2fc7cb3a9bbbb
Bardia Jedi <Bardia.Jedi@enfogroup.com>
2021-09-26 16:10:11 +0200

    added blog cards

---
+++ components/blogCard.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+// import "../styles/blogcard.module.css"
+import styles from '../styles/blogcard.module.css'
+import Image from 'next/image'
+
+
+export default function BlogCard({ href, title, description, thumbnail, metadata }) {
+    return <div className={styles.card} onClick={_ => window.open(href)}>
+        {/* <img className={styles.img} src="images/MSAzure-functions.png" alt="" /> */}
+        <img className={styles.img} src={thumbnail} alt="" />
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.description}>{description}</p>
+        <span className={styles.date}>{metadata}</span>
+    </div>
+}
