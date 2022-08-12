7ba56a661f81788a2eaa073bf7e683e4eee0b51d
Bardia Jedi <bardiajedi@gmail.com>
2022-08-12 11:58:34 +0200

    fix(meta): meta twitter prop name change

--- components/master.jsx
+++ components/master.jsx
@@ -7,11 +7,11 @@ export default function Master({ children }) {
             <title>Bardia Jedi</title>
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             <meta name="description" content="The blog of Bardia Jedi Software developer" />
-            <meta name="image" property="twitter:image" content="https://bardiajedi.com/images/bjIcon.png" />
-            <meta name="image" property="twitter:image" content="https://preview.bardiajedi.com/images/bjIcon.png" />
+            <meta name="twitter:image" content="https://bardiajedi.com/images/bjIcon.png" />
+            <meta name="twitter:image" content="https://preview.bardiajedi.com/images/bjIcon.png" />
             <meta name="twitter:creator" content="@bardiajedi" />
-            <meta name="og:image" content="https://bardiajedi.com/images/bjIcon.png" />
-            <meta name="og:image" content="https://preview.bardiajedi.com/images/bjIcon.png" />
+            <meta name="image" property="og:image" content="https://bardiajedi.com/images/bjIcon.png" />
+            <meta name="image" property="og:image" content="https://preview.bardiajedi.com/images/bjIcon.png" />
             <html lang="en" />
         </Head>
         <NavBar />
