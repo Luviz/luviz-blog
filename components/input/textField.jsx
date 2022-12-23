e24504b3d35df1a6e286f5296b2fe32b56cdfb6a
Bardia Jedi <bardiajedi@gmail.com>
2022-12-23 14:18:31 +0100

    tibber list form

--- components/input/textField.jsx
+++ components/input/textField.jsx
@@ -4,13 +4,17 @@ import styles from "../../styles/inputs.module.css";
 export function TextField({
     placeholder=undefined,
     onChange=undefined,
-    style=undefined
+    style=undefined,
+    value=undefined,
+    tabindex=undefined
 }) {
     return <input 
         type="text"
         className={styles.textField}
         placeholder={placeholder}
         style={style}
+        value={value}
+        tabIndex={tabindex}
         onChange={(e) => {
             if (onChange){
                 onChange(e.currentTarget.value)
--- pages/tibber.jsx
+++ pages/tibber.jsx
@@ -1,32 +1,102 @@
+import { useEffect, useReducer, useState } from "react";
+import useLocalStorage from "use-local-storage";
 import { Button, TextField } from "../components/input";
 
-export default function Tibber() {
+const TibberForm = ({
+  onSubmit = undefined,
+  onDelete = undefined,
+  defaultValues = undefined,
+}) => {
+  const [values, setValues] = useReducer(
+    (state, act) => {
+      return { ...state, ...act };
+    },
+    { ...defaultValues }
+  );
   return (
-    <div style={{
-        margin:'.5rem'
-    }}>
-      <div
-        style={{
-          display: "flex",
-          gap: "1rem",
-          paddingBottom: "0.5rem",
-          
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit && onSubmit(values);
         }}
       >
-        <TextField placeholder={"Label"} onChange={(nv) => console.log(nv)} />
-        <TextField
-          placeholder={"Home index"}
-          onChange={(nv) => console.log(nv)}
-        />
-        <TextField
-          placeholder={"Tibber Key"}
+        <div
           style={{
-              flexGrow: "1"
+            display: "flex",
+            gap: "1rem",
+            padding: "0.25rem 0",
+          }}
+        >
+          <TextField
+            placeholder={"Label"}
+            onChange={(nv) => setValues({ label: nv })}
+            value={values.label}
+          />
+          <TextField
+            placeholder={"Home index"}
+            onChange={(nv) => setValues({ homeIx: nv })}
+            value={values.homeIx}
+          />
+          <TextField
+            placeholder={"Tibber Key"}
+            onChange={(nv) => setValues({ key: nv || "" })}
+            value={values.key}
+            style={{
+              flexGrow: "1",
             }}
-          onChange={(nv) => console.log(nv)}
-        />
-      </div>
-      <Button value={"Add"} />
+          />
+          <Button
+            tabindex={-1}
+            value={"Delete"}
+            onClick={() => onDelete && onDelete(values)}
+          />
+        </div>
+        <button type="submit" hidden />
+      </form>
+    </div>
+  );
+};
+
+export default function Tibber() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [tibberItems, setTibberItems] = useLocalStorage("tibberKeys", []);
+
+  return (
+    <div
+      style={{
+        margin: ".5rem",
+      }}
+    >
+      <div style={{ color: "white" }}>Items without key are removed</div>
+      {[...tibberItems, {key:undefined}].map((item, ix) => {
+        return (
+          <TibberForm
+            key={item.key}
+            defaultValues={{
+              label: item.label,
+              homeIx: item.homeIx,
+              key: item.key,
+            }}
+            onDelete={() => {
+              const tibberCopy = [...tibberItems]
+              const _ = tibberCopy.splice(ix, 1)
+              setTibberItems([]);
+              setTibberItems(tibberCopy);
+            }}
+            onSubmit={(nv) => {
+              setErrorMessage("");
+              const newItems = [...tibberItems];
+              if (nv.key) {
+                newItems[ix] = nv;
+              }
+              setTibberItems(newItems.filter((i) => typeof i === "object"));
+            }}
+          />
+        );
+      })}
+
+      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
     </div>
   );
 }
