83322f4edff549c9d2cb879c2626794aa909ad06
Bardia Jedi <bardiajedi@gmail.com>
2023-04-05 14:26:47 +0200

    feat(counter): added multiplier

--- pages/counter.jsx
+++ pages/counter.jsx
@@ -1,31 +1,50 @@
 import styles from "../styles/counter.module.css";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function Counter() {
   const [count, setCount] = useState(0);
   const [disable, setDisable] = useState(false);
+  const [multi, setMulti] = useState(0)
+  const {format} = new Intl.NumberFormat()
+
   return (
-    <div className={styles.container}>
-      <div className={styles["warning-text"]}>
-        This dose not work on Safari/IOS get a better device!
+    <div>
+      <div>
+        <RenderWarning />
       </div>
-      <div className={styles.text}>{count}</div>
-      <div
-        data-disable={disable}
-        onClick={() => {
-          if (!disable) {
-            setDisable(true);
-            setCount((ps) => ps + 1);
-            if (navigator.vibrate) {
-              navigator.vibrate([200, 50, 400]);
+
+      <div className={styles.container}>
+        <div className={styles['input-container']}>
+          <div className={styles.text}>count: {count}</div>
+          <input type="number" onChange={(e) => setMulti(e.target.value)}/>
+          <div className={styles.text}>{format(count * multi)}</div>
+        </div>
+        <div
+          data-disable={disable}
+          onClick={() => {
+            if (!disable) {
+              setDisable(true);
+              setCount((ps) => ps + 1);
+              if (global?.navigator?.vibrate) {
+                navigator.vibrate([200, 50, 400]);
+              }
             }
-          }
-          setTimeout(() => setDisable(false), 800);
-        }}
-        className={styles.button}
-      >
-        Click Me
+            setTimeout(() => setDisable(false), 800);
+          }}
+          className={styles.button}
+        >
+          Click Me
+        </div>
       </div>
     </div>
   );
 }
+
+const RenderWarning = () => {
+  if (!!global?.navigator?.vibrate) return <></>;
+  return (
+    <div className={styles["warning-text"]}>
+      The vibrate api is not available on you device
+    </div>
+  );
+};
