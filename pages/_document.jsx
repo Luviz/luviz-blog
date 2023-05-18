23dada88d9b73bf9a8c946389fa355b4d4c6a850
Bardia Jedi <bardiajedi@gmail.com>
2023-05-18 21:09:12 +0200

    feat(og): added new meta props

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,11 +6,29 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="twitter:title" content="Bardia Jedi"></meta>
+        <meta property="og:title" content="Bardia Jedi"></meta>
+        <meta
+          property="twitter:description"
+          content="The blog of Bardia Jedi Software developer"
+        ></meta>
         <meta
           name="description"
           content="The blog of Bardia Jedi Software developer"
         />
+        <meta
+          name="og:description"
+          content="The blog of Bardia Jedi Software developer"
+        />
         <meta name="twitter:card" content="summary" />
+        <meta
+          property="og:image"
+          content="https://bardiajedi.com/images/bjIcon.png"
+        ></meta>
+        <meta
+          property="og:image"
+          content="https://preview.bardiajedi.com/images/bjIcon.png"
+        ></meta>
         <meta
           name="twitter:image"
           content="https://bardiajedi.com/images/bjIcon.png"
