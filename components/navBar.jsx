70c2e709c260fc91600ba04d12f2cb354e019c50
Bardia Jedi <bardiajedi@gmail.com>
2023-05-18 17:05:56 +0200

    fix(nav): banner Icon locations

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -1,20 +1,30 @@
 /* eslint-disable @next/next/no-img-element */
-import styles from "../styles/navBar.module.css"
-import Link from "next/link"
+import styles from "../styles/navBar.module.css";
+import Link from "next/link";
 
 export default function NavBar() {
-    const li = [
-        { text: "Home", value: "/" },
-        { text: "Blog", value: "/blog" },
-        { text: "About", value: "/about" },
-    ].map((l, ix) => <li key={ix}><Link href={l.value} passHref>{l.text}</Link></li>)
+  const li = [
+    { text: "Home", value: "/" },
+    { text: "Blog", value: "/blog" },
+    { text: "About", value: "/about" },
+  ].map((l, ix) => (
+    <li key={ix}>
+      <Link href={l.value} passHref>
+        {l.text}
+      </Link>
+    </li>
+  ));
 
-    return <nav className={styles.nav}>
-        <Link href="/" passHref>
-            <img src="/Icon-BJ.svg" alt="Bardia Jedi Logo" className={styles.bannerIcon} />
-        </Link>
-        <ul>
-            {li}
-        </ul>
+  return (
+    <nav className={styles.nav}>
+      <Link className={styles.bannerIconLink} href="/" passHref>
+        <img
+          src="/Icon-BJ.svg"
+          alt="Bardia Jedi Logo"
+          className={styles.bannerIcon}
+        />
+      </Link>
+      <ul>{li}</ul>
     </nav>
+  );
 }
