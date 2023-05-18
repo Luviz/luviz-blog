8596a80c3823dcf8a656f188afae781bb357ca73
Bardia Jedi <bardiajedi@gmail.com>
2023-05-18 23:42:02 +0200

    feat(pwa): init

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
-module.exports = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
-    return config;
+module.exports = withPWA({
+  ...{
+    reactStrictMode: true,
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      });
+      return config;
+    },
   },
-};
+});
